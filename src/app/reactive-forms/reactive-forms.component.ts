c53918985ec0fd151ea5fcc187c2a0a6867a29bc
andriihladkyi96 <andriihladkyi96@gmail.com>
2021-11-15 13:35:28 +0200

    fixed bug with validation

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,6 +1,6 @@
 import { Emploee } from './../models/emploee';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -9,32 +9,63 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ReactiveFormsComponent implements OnInit {
 
-  @Input() numberOfEmployees:number;
+  @Input() numberOfEmployees: number;
   emploees: Emploee[] = [];
   @Output() saveEmploees = new EventEmitter<Emploee[]>();
   displayedColumns: string[] = ['name', 'lastName', 'email', 'phoneNumber', 'position'];
   isEdit: boolean = true;
 
   ngOnInit(): void {
-    this.fillArrayOfEmployees()
+    this.fillArrayOfEmployees();
+    this.fillFormArray();
   }
 
-  emploeeForm: FormGroup = new FormGroup({
-    "emploeeName": new FormControl('', [Validators.required, Validators.pattern("[a-zA-ZА-ЩЬЮЯҐЄІЇа-щьюяґєії][a-zA-ZА-ЩЬЮЯҐЄІЇа-щьюяґєії_]*"), Validators.minLength(2), Validators.maxLength(20)]),
-    "emploeeLastName": new FormControl('', [Validators.required, Validators.pattern("[a-zA-ZА-ЩЬЮЯҐЄІЇа-щьюяґєії][a-zA-ZА-ЩЬЮЯҐЄІЇа-щьюяґєії_]*")]),
-    "emploeePhoneNumber": new FormControl(null, [Validators.pattern('(\\+380[\d]{9})|(0[\d]{9})*')]),
-    "emploeeEmail": new FormControl('', [Validators.required, Validators.email]),
-    "emploeePosition": new FormControl('', [Validators.required, this.userNameValidator])
+  constructor(private fb: FormBuilder) { }
+
+  form = this.fb.group({
+    emploeeArrayForm: this.fb.array([])
   });
 
+  get emploeeArrayForm() : FormArray {
+    return this.form.controls["emploeeArrayForm"] as FormArray;
+  }
+
+  getControls(): FormGroup[] {
+    return (this.form.controls["emploeeArrayForm"] as FormArray).controls as FormGroup[];
+  }
+
+  addEmploeeForm() {
+    const emploeeForm = this.fb.group({
+      "emploeeName": ['', [Validators.required, Validators.pattern("[a-zA-ZА-ЩЬЮЯҐЄІЇа-щьюяґєії][a-zA-ZА-ЩЬЮЯҐЄІЇа-щьюяґєії_]*"), Validators.minLength(2), Validators.maxLength(20)]],
+      "emploeeLastName": ['', [Validators.required, Validators.pattern("[a-zA-ZА-ЩЬЮЯҐЄІЇа-щьюяґєії][a-zA-ZА-ЩЬЮЯҐЄІЇа-щьюяґєії_]*")]],
+      "emploeePhoneNumber": [null,Validators.pattern("^((0[0-9]{9})|(\\+380[0-9]{9}))*")],
+      "emploeeEmail": ['', [Validators.required, Validators.email]],
+      "emploeePosition": ['', [Validators.required, this.userNameValidator]]
+    });
+
+    this.emploeeArrayForm.push(emploeeForm);
+  }
+
+
+  deleteEmploeeForm(lessonIndex: number) {
+    this.emploeeArrayForm.removeAt(lessonIndex);
+  }
+
   submit(id: number) {
-    this.emploees[id].email = this.emploeeForm.value.emploeeEmail;
-    this.emploees[id].name = this.emploeeForm.value.emploeeName;
-    this.emploees[id].lastName = this.emploeeForm.value.emploeeLastName;
-    this.emploees[id].phoneNumber = this.emploeeForm.value.emploeePhoneNumber;
-    this.emploees[id].position = this.emploeeForm.value.emploeePosition;
+    this.emploees[id].email = this.emploeeArrayForm.controls[id].value.emploeeEmail;
+    this.emploees[id].name = this.emploeeArrayForm.controls[id].value.emploeeName;
+    this.emploees[id].lastName = this.emploeeArrayForm.controls[id].value.emploeeLastName;
+    this.emploees[id].phoneNumber = this.emploeeArrayForm.controls[id].value.emploeePhoneNumber;
+    this.emploees[id].position = this.emploeeArrayForm.controls[id].value.emploeePosition;
     this.emploees[id].isEdit = false;
     this.changeEdit();
+  }
+
+  
+  private fillFormArray() {
+    for (let i = 1; i <= this.numberOfEmployees; i++) {
+      this.addEmploeeForm();
+    }
   }
 
   private changeEdit() {
@@ -51,10 +82,10 @@ export class ReactiveFormsComponent implements OnInit {
     return { "emploeePosition": true };
   }
 
-  private fillArrayOfEmployees(){
+  private fillArrayOfEmployees() {
     for (let i = 1; i <= this.numberOfEmployees; i++) {
       this.emploees.push(new Emploee(i, '', '', '', '', '', true))
     }
-}
+  }
 }
 
